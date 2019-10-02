e983c63647fefd289e8a27381d7ddf246671254f
shikata <jamroge1@uat.edu>
2019-10-01 20:18:47 -0700

    Added more detail to the README.md file and commented some more code.

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # mindSeeker
 A Recon automation tool that simplifies the recon process for your needs by running different tools on a host based on the ports that are initially open and reporting the findings in a simplified and effective manner. 
 
+# Usage
+ - sudo ./mindSeeker.sh <IPADDRESS>
+   - CIDR Ranges not supported at this time. Future planning and implementations
+
+# Features
+ - Automatically runs full scope NMAP scans on a target and outputs the results to a organized file structure
+ - Prevents in-efficient scanning by only targeting found open ports with further enumeration
+ - Based on certain open ports on a system. mindSeeker will try to use known tools to enumerate the common service that is usually on that.
+ - 
+ - 
+
+# To Do List
+ - [ ] Allow for the use of CIDR notation inside IP address specifications.
+ - [ ] Add the ability to use previously established file structures or files to tell if the scan had already run against a host or series of hosts. This way scans can be resumed if the are aborted mistakenly.
+   - [ ] Add a optional argument that can be used to point to a mindSeeker file structure
+ - [ ] Implement options to allow the user to run passive recon tools automatically on a domain. 
+ - [ ] Add the command for XML output in all nmap commands.
+ - [ ] Stop blinking text after a process is complete.
+ - [ ] Add support for UDP Scanning and UDP port vuln enum scanning
+ - [ ] Allow the ability for processing of filtered ports
+ - [ ] Ports to add specialized scanning for
+   - [ ] Web Services: 80, 443
+     - [ ] Directory Enumeration
+       - [ ] Add the ability to fuzz web directories
+       - [ ] Output the results to a file 
+   - [ ] SQL
--- mindSeeker.sh
+++ mindSeeker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+'
+Author: Shikata
+Name: MindSeeker
+Version No: 0.1
+:'
 
-
+#ASCII TEXT
 echo -e " ███▄ ▄███▓ ██▓ ███▄    █ ▓█████▄      ██████ ▓█████ ▓█████  ██ ▄█▀▓█████  ██▀███ \n▓██▒▀█▀ ██▒▓██▒ ██ ▀█   █ ▒██▀ ██▌   ▒██    ▒ ▓█   ▀ ▓█   ▀  ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒\n▓██    ▓██░▒██▒▓██  ▀█ ██▒░██   █▌   ░ ▓██▄   ▒███   ▒███   ▓███▄░ ▒███   ▓██ ░▄█ ▒\n▒██    ▒██ ░██░▓██▒  ▐▌██▒░▓█▄   ▌     ▒   ██▒▒▓█  ▄ ▒▓█  ▄ ▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  \n▒██▒   ░██▒░██░▒██░   ▓██░░▒████▓    ▒██████▒▒░▒████▒░▒████▒▒██▒ █▄░▒████▒░██▓ ▒██▒\n░ ▒░   ░  ░░▓  ░ ▒░   ▒ ▒  ▒▒▓  ▒    ▒ ▒▓▒ ▒ ░░░ ▒░ ░░░ ▒░ ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░\n░  ░      ░ ▒ ░░ ░░   ░ ▒░ ░ ▒  ▒    ░ ░▒  ░ ░ ░ ░  ░ ░ ░  ░░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░\n░      ░    ▒ ░   ░   ░ ░  ░ ░  ░    ░  ░  ░     ░      ░   ░ ░░ ░    ░     ░░   ░ \n       ░    ░           ░    ░             ░     ░  ░   ░  ░░  ░      ░  ░   ░     \n                           ░                                                       \n"
 echo "Author-> Shikata"
 
@@ -11,14 +16,14 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-#Check the number of command line arguments
+#Check the number of command line arguments. For not all we need is one and none more. 
 if [ "$#" -ne 1 ]; then
 	echo "Usage: $0 <ipAddress>"
 	exit 1
 fi
 echo -e "\nCheck the mindSeeker directory for your results"
 
-#Create a directory to hold all of the output files that come from this script
+#Create a directory structure to hold all of the output files that come from this script
 mkdir mindSeeker 2>/dev/null
 mkdir mindSeeker/$1 2>/dev/null
 mkdir mindSeeker/$1/nmapScans 2>/dev/null
@@ -27,21 +32,23 @@ mkdir mindSeeker/$1/nmapScans 2>/dev/null
 
 echo ____Peforming Syn Scan on Host $1____
 echo  -e "-->\033[32;5m Please Wait\033[0m"
-#Run a NMAP Syn Scan
+
+#Run a NMAP Syn Scan on the target. Scanning all open ports. Output the results in both grepable format and regular. 
+#       - The piping takes the output from nmap and parses it for so that there is only a ports inside of a file that are found to be open. 
 sudo nmap -sS $1 -p- -oN mindSeeker/$1/nmapScans/synScan -oG mindSeeker/$1/nmapScans/synScan.grep | grep open | cut -d "/" -f 1 > mindSeeker/$1/nmapScans/hostPorts
 echo -e "--> Done\n\n"
 
 
 echo ____Performing TCP Connect Scan on Host $1____
 echo  -e "-->\033[32;5m Please Wait\033[0m"
-#Run a nmap connect scan against the target. 
+#Run a nmap Connect and version scan on the target using the found open ports from the syn scan. Output the results to the file structure.
 nmap -sT -sV --version-intensity 5 -oN mindSeeker/$1/nmapScans/Versionscan -oG mindSeeker/$1/nmapScans/versionScan.grep -p $(tr '\n' , <mindSeeker/$1/nmapScans/hostPorts | sed 's/,*$//g') $1 >/dev/null
 echo -e "--> Done\n\n"
 
 
 echo ____Running Fingerprint Scan on Host $1____
 echo  -e "-->\033[32;5m Please Wait\033[0m"
-#Runs a Fingerprint Scan Against the target
+#Runs a Fingerprint Scan Against the target on the found open ports during the syn scan. Outputs the results to the file structure.
 nmap -A -oN mindSeeker/$1/nmapScans/fingerprintScan -oG mindSeeker/$1/nmapScans/fingerprintScan.grep -p $(tr '\n' , <mindSeeker/$1/nmapScans/hostPorts | sed 's/,*$//g') $1 >/dev/null
 echo -e "--> Done\n\n"
 
@@ -51,7 +58,7 @@ mkdir mindSeeker/$1/nmapScans/vulnScans 2>/dev/null
 
 
 
-#Automatically performs Vuln Enumerations
+#Automatically performs Vuln Enumerations on the found open ports inside the 
 for openPort in $(cat mindSeeker/$1/nmapScans/hostPorts);
 do
         echo  -e "Checking Port: \033[32;5m$openPort\033[0m"
@@ -69,7 +76,7 @@ do
 	
 	#Scans for SMTP Vulns
 	if [ $openPort -eq 25 ] || [ $openPort -eq 587 ] || [ $openPort -eq 465 ]; then
-                nmap -p $openPort -oN mindSeeker/$1/nmapScans/vulnScans/SMTP -oG mindSeeker/$1/nmapScans/vulnScans/SMTP.grep --script=smtp-vuln*,smtp-enum-users,smtp-ntlm-info,smtp-open-relay $1 > /dev/null
+                nmap -p $openPort -oN mindSeeker/$1/nmapScans/vulnScans/SMTP -oG mindSeeker/$1/nmapScans/vulnScans/SMTP.grep --script=smtp-vuln*,smtp-enum-users,smtp-ntlm-info, $1 > /dev/null
         fi
 	
 

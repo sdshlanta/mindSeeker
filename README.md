44dfa48912cbc51afe23bc1ae991afd97b2ac6e2
shikata <jamroge1@uat.edu>
2019-10-03 19:22:12 -0700

    Changes to be committed: 	modified:   README.md 	modified:   mindSeeker.py ALERTS: 	- Bash code is being phased out. Python code will take precedence on next update
    Summary of Changes:
    	- Threading is now working inside the python source file
    	    - Added a flag in which the user can specify how many threads they want to run on the machine. Defaults to ten
    	    - Codebase will only run the specifed number of threads at once for a process.
    	    - Threads that are running on scans that are on hosts that are offline will now terminate
    
    	- Printing of the state of each thread is now implemented inside the code. This allows for proper tracking of where each scan is
    	    - Figured out how to print while recycling the same lines using ANSI code. There are some bugs in this but it is functioning -> http://tldp.org/HOWTO/Bash-Prompt-HOWTO/x361.html
    	    - Printing uses a global dictionary to track Threads to States.
    
    Version 1:
    	- Vulnerability enumeration of ports is implemented
    	- Blinky text is implemented for printing.
    	- Printing bug with large subnets to be investigated

--- README.md
+++ README.md
@@ -1,19 +1,36 @@
 # mindSeeker
 A Recon automation tool that simplifies the recon process for your needs by running different tools on a host based on the ports that are initially open and reporting the findings in a simplified and effective manner. 
 
+ - MindSeeker.sh is being phased out currently. 
+
 # Usage
- - sudo ./mindSeeker.sh <IPADDRESS>
-   - CIDR Ranges not supported at this time. Future planning and implementations
+```
+usage: mindSeeker.py [-h] (--ipAddress IPADDRESS/CIDR | --dns DNS)
+                     [--resume RESUME] [--maxThreads MAXTHREADS]
+
+Description Placeholder
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --ipAddress IPADDRESS
+                        Takes the IP Address or range of ip addresses to scan
+  --dns DNS             Takes the DNS Address of the server to scan
+  --resume RESUME       Used to resume a mindSeeker scan. Point it too the
+                        directory where mindSeeker is stored.
+  --maxThreads MAXTHREADS Adjusts the max number of threads that can run
+```
+ - You can only supply an ipAddress/CIDR or a DNS address. Not both
+ - You can input CIDR ranges with ip addresses to scan a range of nodes. 
 
 # Features
  - Automatically runs full scope NMAP scans on a target and outputs the results to a organized file structure
  - Prevents in-efficient scanning by only targeting found open ports with further enumeration
  - Based on certain open ports on a system. mindSeeker will try to use known tools to enumerate the common service that is usually on that.
- - 
- - 
 
 # To Do List
- - [ ] Allow for the use of CIDR notation inside IP address specifications.
+ - [X] Allow for the use of CIDR notation inside IP address specifications.
+ - [X] Add Threading support to the python code
+ - [ ] Convert the codebase to python
  - [ ] Add the ability to use previously established file structures or files to tell if the scan had already run against a host or series of hosts. This way scans can be resumed if the are aborted mistakenly.
    - [ ] Add a optional argument that can be used to point to a mindSeeker file structure
  - [ ] Implement options to allow the user to run passive recon tools automatically on a domain. 
--- mindSeeker.py
+++ mindSeeker.py
@@ -10,10 +10,15 @@ import stat
 import argparse 
 import socket
 import threading
+import time
+import shutil
 from netaddr import *
+import signal
 
 #Global Constants
 FILE_STRUCTURE_ROOT_NAME = 'mindSeeker'
+
+
 
 #command line arguments
 parser = argparse.ArgumentParser()
@@ -30,34 +35,80 @@ group.add_argument('--dns', help="Takes the DNS Address of the server to scan")
 
 #More Arguments 
 parser.add_argument('--resume', help="Used to resume a mindSeeker scan. Point it too the directory where mindSeeker is stored.")
-
+parser.add_argument('--maxThreads', help="Adjusts the max number of threads that can run", default=10)
 args = parser.parse_args()
 
+#Global Variables
+threadStateDictionary = {}
+#Sets the max amount of threads to the inputted max threads
+semaphore = threading.BoundedSemaphore(value=int(args.maxThreads))
 
 #Prints the banner for the mindSeeker code
 def banner():
     print(" ███▄ ▄███▓ ██▓ ███▄    █ ▓█████▄      ██████ ▓█████ ▓█████  ██ ▄█▀▓█████  ██▀███ \n▓██▒▀█▀ ██▒▓██▒ ██ ▀█   █ ▒██▀ ██▌   ▒██    ▒ ▓█   ▀ ▓█   ▀  ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒\n▓██    ▓██░▒██▒▓██  ▀█ ██▒░██   █▌   ░ ▓██▄   ▒███   ▒███   ▓███▄░ ▒███   ▓██ ░▄█ ▒\n▒██    ▒██ ░██░▓██▒  ▐▌██▒░▓█▄   ▌     ▒   ██▒▒▓█  ▄ ▒▓█  ▄ ▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  \n▒██▒   ░██▒░██░▒██░   ▓██░░▒████▓    ▒██████▒▒░▒████▒░▒████▒▒██▒ █▄░▒████▒░██▓ ▒██▒\n░ ▒░   ░  ░░▓  ░ ▒░   ▒ ▒  ▒▒▓  ▒    ▒ ▒▓▒ ▒ ░░░ ▒░ ░░░ ▒░ ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░\n░  ░      ░ ▒ ░░ ░░   ░ ▒░ ░ ▒  ▒    ░ ░▒  ░ ░ ░ ░  ░ ░ ░  ░░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░\n░      ░    ▒ ░   ░   ░ ░  ░ ░  ░    ░  ░  ░     ░      ░   ░ ░░ ░    ░     ░░   ░ \n       ░    ░           ░    ░             ░     ░  ░   ░  ░░  ░      ░  ░   ░     \n                           ░                                                       \n")
 
-def nmapScans(ipAddress):
-    #Create the file for the ip Address we are scanning
-    os.mkdir(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress)
-    os.mkdir(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans")
+#Used for setting the status of the thread inside the dictionary for printing
+def threadStatus(val):
+    threadStateDictionary[threading.current_thread().getName()] = val
 
+def nmapScans(ipAddress):
+
+    #Create the file for the ip Address we are scanning
+    os.makedirs(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress, exist_ok=True)
+    os.makedirs(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans", exist_ok=True)
     os.system("nmap -sS " + ipAddress + " -p- -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/synScan -oG "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/synScan.grep | grep open | cut -d \"/\" -f 1 > "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/hostPorts")
+    
+    #If the file for host ports contains nothing than we know the host is not online so we break the thread for this IP
+    if(os.stat(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/hostPorts").st_size == 0):
+        threadStatus("-1")
+        #remove the directory tree for the thread
+        shutil.rmtree(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress,ignore_errors=True)
+        #Returns are used for deciding or not whether the thread should exit
+        return 0 
+
+    
+    threadStatus("Running Connect Scan")
+
     os.system("nmap -sT -sV --version-intensity 5 -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/Versionscan -oG mindSeeker/"+ipAddress+"/nmapScans/versionScan.grep -p $(tr '\n' , <"+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/hostPorts | sed 's/,*$//g') "+ipAddress+" >/dev/null")
+    threadStatus("Running Fingerprinting Scan")
+
     os.system("nmap -A -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/fingerprintScan -oG "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/fingerprintScan.grep -p $(tr '\\n' , <"+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/hostPorts | sed 's/,*$//g') "+ipAddress+" >/dev/null")
 
-def portVulNEnum(ipAddress):
-    with open("mindSeeker/"+ipAddress+"/nmapScans/hostPorts") as portFile:
-        line = portFile.readline()
-        print(line)
+    #Returns are used for deciding whether or not the thread shoudl exit
+    return 1
+    
 
+def portVulNEnum(ipAddress):
+    try:
+        file = open("mindSeeker/"+ipAddress+"/nmapScans/hostPorts")
+        for line in file:
+            threadStatus("Assessing Port: " + line.strip())
+            if(line.strip() == "20"):
+                print("True")
+            elif(line.strip() == "20"):
+                print("False")
+        file.close()
+    except:
+        print("Error: hostPorts file not found ")
 
 
 #Threading Function that is used to called by threads. 
 def enumHost(ipAddress):
-    nmapScans(ipAddress)
-    portVulNEnum(ipAddress)
+
+    #Sephamores only allow a certain amount of threads to take control of a process. Only 10 threads will be allowed to run at a time
+    semaphore.acquire(blocking=True)
+
+    #Adds the current threads name and state to the dictionary
+    threadStatus("Running Syn Scan")
+
+    #If the function doesnt return one we dont run any of the other processes
+    if(nmapScans(ipAddress) == 1):
+        portVulNEnum(ipAddress)
+    
+    threadStatus("Finished with Evaluation.")
+
+
+    semaphore.release()
 
 def resume():
     #Resume feature to be implemented
@@ -93,21 +144,32 @@ def main():
         resume()
 
     #If a Cidr range is used. Removes the first IP address which is invalid. 
-
     for i in range(len(ipData)):
-        thread = threading.Thread(target=enumHost,args=(str(ipData[i]),))
+        thread = threading.Thread(name=str(ipData[i]), target=enumHost,args=(str(ipData[i]),))
         threads.append(thread)
+        #Handles the creation of values within our dictionarty for printing
+        
 
+    print("Here")
     for thread in threads:
         thread.start()
+    print("here1")
     
+    #This series of lines handles the printing associated with the threads
+    while(threading.active_count() > 1):
+        printedLines = 0
+        for i in range(len(threadStateDictionary)):
+            print(threads[i].name + " : " + threadStateDictionary[threads[i].name] + " "*10)
+            #print(threads[i].name + " " + threadStateDictionary[threads[i].name])
+            printedLines=printedLines + 1
+
+        time.sleep(1)
+        #http://tldp.org/HOWTO/Bash-Prompt-HOWTO/x361.html
+        print("\033["+str(printedLines+1)+"A")
+
     for thread in threads:
         thread.join()
 
 
 if __name__ == "__main__":
     main()
-
-'''
-
-'''

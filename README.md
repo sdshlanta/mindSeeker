fa5ed12a964ff3dfbb1b98ff91bbe1f4759204cb
shikata <jamroge1@uat.edu>
2019-10-04 21:53:05 -0700

    Changes to be committed: 	modified:   README.md 	modified:   mindSeeker.py ALERTS: 	- Bash code is being phased out. Python code will take precedence on next update
    Summary of Changes:
    	- Got blinking text properly working. Almost all printing has really good printing systems except for port enumerations which needs to be adjusted
    	- Finished converting the old bash codebase to python. Python code now is able to perform vulnerability scans based on ports after initial nmapping.
    	- Added a counter that tracks the offline hosts. Offline hosts are printed as a number instead of having a line for each IP
    
    Bug Fixes:
    	- Fixed a few printing bugs that were causing confusing output
    	- Fixed a bug that caused scanning to be performed on a invalid ip address when using CIDR ranges

--- README.md
+++ README.md
@@ -30,8 +30,16 @@ optional arguments:
 # To Do List
  - [X] Allow for the use of CIDR notation inside IP address specifications.
  - [X] Add Threading support to the python code
- - [ ] Convert the codebase to python
+ - [ ] Implement a verbose output option that allows a user to see debugged output from the program
+ - [X] Convert the codebase to python
+   - [X] Properly Printed and Organized Threads 
+   - [X] Color Coded Output
+   - [X] Perform full scope nmap scans on each target
+   - [X] Properly blinking text
+ - [ ] Make highlighted ports being assessed blink
  - [ ] Add the ability to use previously established file structures or files to tell if the scan had already run against a host or series of hosts. This way scans can be resumed if the are aborted mistakenly.
+ - [ ] Allow comma seperated IP address lists
+ - [ ] allow dashed seperated ip address ranges
    - [ ] Add a optional argument that can be used to point to a mindSeeker file structure
  - [ ] Implement options to allow the user to run passive recon tools automatically on a domain. 
  - [ ] Add the command for XML output in all nmap commands.
@@ -39,8 +47,12 @@ optional arguments:
  - [ ] Add support for UDP Scanning and UDP port vuln enum scanning
  - [ ] Allow the ability for processing of filtered ports
  - [ ] Ports to add specialized scanning for
+ - [ ] Use Sockets for raw syn scanning instead of NMAP syn scanning to make initial evaluations faster
+ - [ ] Add a thread that comes online when all other threads are used up. This thread will go ahead of the others and try to evaluate offline hosts
    - [ ] Web Services: 80, 443
      - [ ] Directory Enumeration
        - [ ] Add the ability to fuzz web directories
        - [ ] Output the results to a file 
+     - [ ] Vulnerability Scanning
+       - [X] Nikto
    - [ ] SQL
--- mindSeeker.py
+++ mindSeeker.py
@@ -1,7 +1,7 @@
 '''
 Program: Mind Seeker
 Author: Shikata
-Version: 0.15
+Version: 1.0
 '''
 
 #!usr/bin/python
@@ -17,14 +17,14 @@ import signal
 
 #Global Constants
 FILE_STRUCTURE_ROOT_NAME = 'mindSeeker'
-
-
+PORTS_TO_VULN_ENUM = ["20","22","23","80","110","135","137","139","143","389","443","445","631","1133","1134","3124","3128","3306","3389",
+"4333","5432","5500","6000","6001","6665","6666","6667","6668","6669","8080"]
 
 #command line arguments
 parser = argparse.ArgumentParser()
 
 #Adds a programs description
-parser = argparse.ArgumentParser(description="Description HEre")
+parser = argparse.ArgumentParser(description="Placeholder")
 
 #Creates a group parser. Only allows one argument inside the group parser to be inputted
 group = parser.add_mutually_exclusive_group(required=True)
@@ -34,7 +34,7 @@ group.add_argument('--ipAddress', help="Takes the IP Address or range of ip addr
 group.add_argument('--dns', help="Takes the DNS Address of the server to scan")
 
 #More Arguments 
-parser.add_argument('--resume', help="Used to resume a mindSeeker scan. Point it too the directory where mindSeeker is stored.")
+parser.add_argument('--resume', help="Used to resume a mindSeeker scan. Point it too the directory where mindSeeker is stored. -> Currently not functioning")
 parser.add_argument('--maxThreads', help="Adjusts the max number of threads that can run", default=10)
 args = parser.parse_args()
 
@@ -46,6 +46,7 @@ semaphore = threading.BoundedSemaphore(value=int(args.maxThreads))
 #Prints the banner for the mindSeeker code
 def banner():
     print(" ███▄ ▄███▓ ██▓ ███▄    █ ▓█████▄      ██████ ▓█████ ▓█████  ██ ▄█▀▓█████  ██▀███ \n▓██▒▀█▀ ██▒▓██▒ ██ ▀█   █ ▒██▀ ██▌   ▒██    ▒ ▓█   ▀ ▓█   ▀  ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒\n▓██    ▓██░▒██▒▓██  ▀█ ██▒░██   █▌   ░ ▓██▄   ▒███   ▒███   ▓███▄░ ▒███   ▓██ ░▄█ ▒\n▒██    ▒██ ░██░▓██▒  ▐▌██▒░▓█▄   ▌     ▒   ██▒▒▓█  ▄ ▒▓█  ▄ ▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  \n▒██▒   ░██▒░██░▒██░   ▓██░░▒████▓    ▒██████▒▒░▒████▒░▒████▒▒██▒ █▄░▒████▒░██▓ ▒██▒\n░ ▒░   ░  ░░▓  ░ ▒░   ▒ ▒  ▒▒▓  ▒    ▒ ▒▓▒ ▒ ░░░ ▒░ ░░░ ▒░ ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░\n░  ░      ░ ▒ ░░ ░░   ░ ▒░ ░ ▒  ▒    ░ ░▒  ░ ░ ░ ░  ░ ░ ░  ░░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░\n░      ░    ▒ ░   ░   ░ ░  ░ ░  ░    ░  ░  ░     ░      ░   ░ ░░ ░    ░     ░░   ░ \n       ░    ░           ░    ░             ░     ░  ░   ░  ░░  ░      ░  ░   ░     \n                           ░                                                       \n")
+    print("\n\nAuthor: Shikata")
 
 #Used for setting the status of the thread inside the dictionary for printing
 def threadStatus(val):
@@ -57,7 +58,7 @@ def nmapScans(ipAddress):
     os.makedirs(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress, exist_ok=True)
     os.makedirs(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans", exist_ok=True)
     os.system("nmap -sS " + ipAddress + " -p- -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/synScan -oG "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/synScan.grep | grep open | cut -d \"/\" -f 1 > "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/hostPorts")
-    
+
     #If the file for host ports contains nothing than we know the host is not online so we break the thread for this IP
     if(os.stat(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/hostPorts").st_size == 0):
         threadStatus("-1")
@@ -69,32 +70,109 @@ def nmapScans(ipAddress):
     
     threadStatus("Running Connect Scan")
 
-    os.system("nmap -sT -sV --version-intensity 5 -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/Versionscan -oG mindSeeker/"+ipAddress+"/nmapScans/versionScan.grep -p $(tr '\n' , <"+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/hostPorts | sed 's/,*$//g') "+ipAddress+" >/dev/null")
+    os.system("nmap -sT -sV --version-intensity 5 -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/Versionscan -oG mindSeeker/"+ipAddress+"/nmapScans/versionScan.grep -p $(tr '\n' , <"+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/hostPorts | sed 's/,*$//g') "+ipAddress+" 2>/dev/null 1>/dev/null")
     threadStatus("Running Fingerprinting Scan")
 
-    os.system("nmap -A -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/fingerprintScan -oG "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/fingerprintScan.grep -p $(tr '\\n' , <"+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/hostPorts | sed 's/,*$//g') "+ipAddress+" >/dev/null")
+    os.system("nmap -A -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/fingerprintScan -oG "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/fingerprintScan.grep -p $(tr '\\n' , <"+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/hostPorts | sed 's/,*$//g') "+ipAddress+" 2>/dev/null 1>/dev/null")
 
     #Returns are used for deciding whether or not the thread shoudl exit
     return 1
     
-
+#for each port inside the file...see if we can't do a bit of vulnerability enumeration on this port. 
 def portVulNEnum(ipAddress):
+    os.makedirs(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/vulnScans", exist_ok=True)
+    portsThatCouldNotBeScanned = []
+    #Figure out a way to better too check if a port has been scanned....Im just really lazy right now and not creative hur dur 
+    httpRan = False
+    smbRan = False
     try:
         file = open("mindSeeker/"+ipAddress+"/nmapScans/hostPorts")
+        #There has to be a way to make this better beside just mass if statements / switch stuff. 
         for line in file:
-            threadStatus("Assessing Port: " + line.strip())
-            if(line.strip() == "20"):
-                print("True")
-            elif(line.strip() == "20"):
-                print("False")
+            #If the port is in the port we are currently enumerating is in the list of ports we can enumerate in our code
+            if(line.strip() in PORTS_TO_VULN_ENUM):
+                threadStatus("\033[36;3mAssessing Port:\033[0m\033[31;7m" + line.strip()+"\033[0m")
+                #Scans for vulnerabilities on FTP
+                if(line.strip() == "20"):
+                    os.system("nmap -p "+line.strip()+" -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/vulnScans/ftp -oG "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/vulnScans/ftp.grep --script=ftp-anon.nse "+ipAddress+" 1>/dev/null 2>/dev/null")
+                #Scans for vulnerabilities on ssh
+                elif(line.strip() == "22"):
+                    os.system("nmap -p "+line.strip()+" -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/vulnScans/ssh -oG "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/vulnScans/ssh.grep --script=sshv1.nse "+ipAddress+" 1>/dev/null 2>/dev/null")
+                #Scans for telnet vulnerabilities
+                elif(line.strip() == "23"):
+                     os.system("nmap -p "+line.strip()+" -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/vulnScans/telnet -oG "+ FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/vulnScans/telnet.grep --script=telnet-ntlm-info "+ipAddress+" 1>/dev/null 2>/dev/null")
+                #Scans for vulnerabilities on SMTP
+                elif(line.strip() == "25"):
+                    null = False
+                #Scans or vulnerabilities on HTTP or HTTPS
+                elif(line.strip() == "80" or line.strip() == "443" and httpRan == False):
+                    os.makedirs(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/niktoScan", exist_ok=True)
+                    os.system("nikto -h "+ipAddress+" -p 80,443 -Format htm -output "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/niktoScan/nikto.html > /dev/null")
+                    httpRan = True
+                #Scans for vulnerability on POP3
+                elif(line.strip() == "110"):
+                    null = False
+                #Scans for SMB and NETBIOS vulnerabilities
+                elif(line.strip() == "135" or line.strip() == "137" or line.strip() == "139" or line.strip() == "445" and smbRan == False):
+                    os.makedirs(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/enum4linux", exist_ok=True)
+                    os.system("nmap -p 135,137,139,445 -oN "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/vulnScans/smb -oG "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/nmapScans/vulnScans/smb.grep --script=smb-enum-shares.nse,smb-vuln-*,smb-protocols "+ipAddress)
+                    os.system("enum4linux -a "+ipAddress+" > "+FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/enum4linux/scan.txt")
+
+                #Scans for vulnerabilities on IMAP4
+                elif(line.strip() == "143"):
+                    null = False
+                #Scans for vulnerabilities on LDAP
+                elif(line.strip() == "389"):
+                    null = False
+                elif(line.strip() == "631"):
+                    null = False
+                #Scans for vulnerabilities on Microsoft SQL
+                elif(line.strip() == "1133" or line.strip() == "1134"):
+                    null = False
+                #Scans for MySQL Vulnerabilities
+                elif(line.strip() == "3306"):
+                    null = False
+                #Scans for vulnerabilities on RDP
+                elif(line.strip() == "3389"):
+                    null = False
+                #Scans for vulnerabilities on mSQL
+                elif(line.strip() == "4333"):
+                    null = False
+                elif(line.strip() == "5432"):
+                    null = False
+                #Scans for vulnerabilities on VNC Server
+                elif(line.strip() == "5500"):
+                    null = False
+                elif(line.strip() == "6000" or line.strip() == "6001"):
+                    null = False
+                #Scans for vulnerabilities on HTTP proxy
+                elif(line.strip() == "8080" or line.strip() == "3124" or line.strip() == "3128"):
+                    null = False
+                #Scans for vulnerabilities inside IRC relates things
+                elif(line.strip() == "6665" or line.strip() == "6666" or line.strip() == "6667" or line.strip() == "6668" or line.strip() == "6669"):
+                    null = False
+            
+            else:
+                #If the port could not be scanned we add it to the list of ports that could not be scanned. 
+                portsThatCouldNotBeScanned.append(line.strip())
+
         file.close()
+        
+        #If there are ports we could not scan. We append them to a file.
+        if(len(portsThatCouldNotBeScanned) > 0):
+            file = open(FILE_STRUCTURE_ROOT_NAME+"/"+ipAddress+"/portsThatWereNotScanned.haha", 'a')
+            for port in portsThatCouldNotBeScanned:
+                file.write(port.strip() + "\n")
+
+            file.close()
+        
     except:
         print("Error: hostPorts file not found ")
 
 
 #Threading Function that is used to called by threads. 
 def enumHost(ipAddress):
-
+    #Wow...this finally works...horay
     #Sephamores only allow a certain amount of threads to take control of a process. Only 10 threads will be allowed to run at a time
     semaphore.acquire(blocking=True)
 
@@ -102,10 +180,12 @@ def enumHost(ipAddress):
     threadStatus("Running Syn Scan")
 
     #If the function doesnt return one we dont run any of the other processes
-    if(nmapScans(ipAddress) == 1):
+    returnVal = nmapScans(ipAddress)
+    if(returnVal == 1):
         portVulNEnum(ipAddress)
-    
-    threadStatus("Finished with Evaluation.")
+        threadStatus("Finished with Evaluation.")
+    else:
+        threadStatus("Host is not online")
 
 
     semaphore.release()
@@ -132,6 +212,11 @@ def main():
     #Ip data object contains all the ip address data
     ipData = IPNetwork(args.ipAddress)
 
+    #If there is a CIDR range. We remove the first ip address as it is invalid broadcast IP.
+    if("/" in args.ipAddress):
+        ipData = list(ipData[1:])
+
+    #Yea this stuff isnt gonna be worked on for awhile...get over it
     #Manages and Creates file structure for the program output to use
     if not args.resume:
         if not (os.path.exists(FILE_STRUCTURE_ROOT_NAME)):
@@ -148,7 +233,7 @@ def main():
         thread = threading.Thread(name=str(ipData[i]), target=enumHost,args=(str(ipData[i]),))
         threads.append(thread)
         #Handles the creation of values within our dictionarty for printing
-        
+    
 
     print("Here")
     for thread in threads:
@@ -157,16 +242,40 @@ def main():
     
     #This series of lines handles the printing associated with the threads
     while(threading.active_count() > 1):
+        offlineHosts = 0
         printedLines = 0
         for i in range(len(threadStateDictionary)):
-            print(threads[i].name + " : " + threadStateDictionary[threads[i].name] + " "*10)
+            #If a host is marked as not online we incremement the offline hosts counter. 
+            if(threadStateDictionary[threads[i].name].strip() == "Host is not online"):
+                offlineHosts = offlineHosts + 1
+            else:
+                #Work on the redundancy of these lines
+                if(threadStateDictionary[threads[i].name].strip() == "Running Syn Scan"):
+                    print("\033[32;4m"+threads[i].name + "\033[0m:\033[33;5m " + threadStateDictionary[threads[i].name] + "\033[0m" + " "*10)
+                    printedLines=printedLines + 1
+                elif(threadStateDictionary[threads[i].name].strip() == "Running Connect Scan"):
+                    print("\033[32;4m"+threads[i].name + "\033[0m:\033[35;5m " + threadStateDictionary[threads[i].name] + "\033[0m" + " "*10)
+                    printedLines=printedLines + 1
+                elif(threadStateDictionary[threads[i].name].strip() == "Running Fingerprinting Scan"):
+                    print("\033[32;4m"+threads[i].name + "\033[0m:\033[31;5m " + threadStateDictionary[threads[i].name] + "\033[0m" + " "*10)
+                    printedLines=printedLines + 1
+                elif(threadStateDictionary[threads[i].name].strip() == "Finished with Evaluation."):
+                    print("\033[32;4m"+threads[i].name + "\033[0m:\033[34;4m " + threadStateDictionary[threads[i].name] + "\033[0m" + " "*10)
+                    printedLines=printedLines + 1
+                else:
+                    print("\033[32;4m"+threads[i].name + "\033[0m: " + threadStateDictionary[threads[i].name] + " "*10)
+                    printedLines=printedLines + 1
             #print(threads[i].name + " " + threadStateDictionary[threads[i].name])
-            printedLines=printedLines + 1
+                    
 
         time.sleep(1)
         #http://tldp.org/HOWTO/Bash-Prompt-HOWTO/x361.html
-        print("\033["+str(printedLines+1)+"A")
+        #Move the cursor up the amount of lines printed + 3
+        print("\033["+str(printedLines+2)+"A") #I cant beleive this works. Holy crap...
+        #prints the total amount of offline hosts above everything else.
+        print("Offline Hosts: " + str(offlineHosts))
 
+    #Not sure if we need this line...might be obsolete because of the while loop. Further research needed.
     for thread in threads:
         thread.join()
 
